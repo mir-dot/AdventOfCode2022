3f2d6ed6403a29aaad2236dbb1edfb46a9ed3539
MiraYHakansson <mira.yao.hakansson@yaytrade.com>
2022-12-02 23:02:09 +0100

    Add project files.

---
+++ Solutions/Day1.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Reflection;
+
+int counter = 0;
+List<int> weights = new List<int>();
+
+string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Inputs\input01.txt");
+string[] lines = File.ReadAllLines(path);
+
+
+foreach (string line in lines)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        weights.Add(counter);
+        counter = 0;
+    }
+    else
+    {
+        counter += int.Parse(line);
+    }
+
+}
+weights.Sort();
+
+int mostCalories = weights.Last();
+
+Console.WriteLine("The elf carrying most calories: {0}", mostCalories);
+Console.WriteLine("The three with most calories: {0}", mostCalories + weights[weights.Count - 2] + weights[weights.Count - 3]);
